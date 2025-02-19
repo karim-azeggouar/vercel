60c808c734d76adf42b5413e3c86166c055911fb
Azeggouar <k.azeggouar@gmail.com>
2025-02-19 10:22:24 +0100

    second commit

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
 // Définition de la route principale
 app.get("/", (req, res) => {
   res.send("Welcome to Vercel Node.js!");
 });
 
-// Démarrage du serveur en local uniquement
-if (require.main === module) {
-  
+app.get("/test", (req, res) => {
+    res.send("Welcome to Vercel Node.js! test");
+  }); 
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
-}
+
 
 // Exportation pour Vercel (nécessaire pour le déploiement)
 module.exports = app;
